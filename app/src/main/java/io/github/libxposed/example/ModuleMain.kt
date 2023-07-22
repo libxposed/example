ca758eead5c69623545d0779bdaa65dc01573661
Nullptr <noreply@nullptr.icu>
2023-07-22 17:16:24 +0800

    New hook API

--- app/src/main/java/io/github/libxposed/example/ModuleMain.kt
+++ app/src/main/java/io/github/libxposed/example/ModuleMain.kt
@@ -4,14 +4,46 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 import io.github.libxposed.api.XposedContext
+import io.github.libxposed.api.XposedInterface
 import io.github.libxposed.api.XposedModule
 import io.github.libxposed.api.XposedModuleInterface.ModuleLoadedParam
 import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
+import io.github.libxposed.api.annotations.AfterInvocation
+import io.github.libxposed.api.annotations.BeforeInvocation
+import io.github.libxposed.api.annotations.XposedHooker
+import java.lang.reflect.Member
+import kotlin.random.Random
+
+private lateinit var module: ModuleMain
 
 class ModuleMain(base: XposedContext, param: ModuleLoadedParam) : XposedModule(base, param) {
 
     init {
         log("ModuleMain at " + param.processName)
+        module = this
+    }
+
+    @XposedHooker
+    class MyHooker(private val magic: Int) : XposedInterface.Hooker {
+        companion object {
+            @JvmStatic
+            @BeforeInvocation
+            fun beforeInvocation(method: Member, thisObject: Any?, args: Array<out Any>): XposedInterface.Hooker {
+                val key = Random.nextInt()
+                val appContext = args[0] as Context
+                module.log("beforeInvocation: key = $key")
+                module.log("app context: $appContext")
+                module.log("xposed context: $this")
+                return MyHooker(key)
+            }
+
+            @JvmStatic
+            @AfterInvocation
+            fun afterInvocation(extras: XposedInterface.Hooker, result: Any?) {
+                val hooker = extras as MyHooker
+                module.log("afterInvocation: key = ${hooker.magic}")
+            }
+        }
     }
 
     @SuppressLint("DiscouragedPrivateApi")
@@ -26,10 +58,6 @@ class ModuleMain(base: XposedContext, param: ModuleLoadedParam) : XposedModule(b
         if (!param.isFirstPackage) return
 
         val exampleMethod = Application::class.java.getDeclaredMethod("attach", Context::class.java)
-        hookBefore(exampleMethod) { callback ->
-            val appContext = callback.args[0] as Context
-            log("app context: $appContext")
-            log("xposed context: $this")
-        }
+        hook(exampleMethod, MyHooker::class.java)
     }
 }
