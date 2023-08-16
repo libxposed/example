3e56879481c0c89afb898b4ef0b323948a2d1a3d
Nullptr <noreply@nullptr.icu>
2023-08-16 09:58:26 +0800

    Change API

--- app/src/main/java/io/github/libxposed/example/ModuleMain.kt
+++ app/src/main/java/io/github/libxposed/example/ModuleMain.kt
@@ -3,20 +3,20 @@ package io.github.libxposed.example
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
-import io.github.libxposed.api.XposedContext
 import io.github.libxposed.api.XposedInterface
+import io.github.libxposed.api.XposedInterface.AfterHookCallback
+import io.github.libxposed.api.XposedInterface.BeforeHookCallback
 import io.github.libxposed.api.XposedModule
 import io.github.libxposed.api.XposedModuleInterface.ModuleLoadedParam
 import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
 import io.github.libxposed.api.annotations.AfterInvocation
 import io.github.libxposed.api.annotations.BeforeInvocation
 import io.github.libxposed.api.annotations.XposedHooker
-import java.lang.reflect.Member
 import kotlin.random.Random
 
 private lateinit var module: ModuleMain
 
-class ModuleMain(base: XposedContext, param: ModuleLoadedParam) : XposedModule(base, param) {
+class ModuleMain(base: XposedInterface, param: ModuleLoadedParam) : XposedModule(base, param) {
 
     init {
         log("ModuleMain at " + param.processName)
@@ -28,20 +28,18 @@ class ModuleMain(base: XposedContext, param: ModuleLoadedParam) : XposedModule(b
         companion object {
             @JvmStatic
             @BeforeInvocation
-            fun beforeInvocation(method: Member, thisObject: Any?, args: Array<out Any>): XposedInterface.Hooker {
+            fun beforeInvocation(callback: BeforeHookCallback): MyHooker {
                 val key = Random.nextInt()
-                val appContext = args[0] as Context
+                val appContext = callback.args[0]
                 module.log("beforeInvocation: key = $key")
                 module.log("app context: $appContext")
-                module.log("xposed context: $this")
                 return MyHooker(key)
             }
 
             @JvmStatic
             @AfterInvocation
-            fun afterInvocation(extras: XposedInterface.Hooker, result: Any?) {
-                val hooker = extras as MyHooker
-                module.log("afterInvocation: key = ${hooker.magic}")
+            fun afterInvocation(callback: AfterHookCallback, context: MyHooker) {
+                module.log("afterInvocation: key = ${context.magic}")
             }
         }
     }
@@ -50,9 +48,8 @@ class ModuleMain(base: XposedContext, param: ModuleLoadedParam) : XposedModule(b
     override fun onPackageLoaded(param: PackageLoadedParam) {
         super.onPackageLoaded(param)
         log("onPackageLoaded: " + param.packageName)
-        log("main classloader is " + this.classLoader)
         log("param classloader is " + param.classLoader)
-        log("module apk path: " + this.packageCodePath)
+        log("module apk path: " + this.applicationInfo.sourceDir)
         log("----------")
 
         if (!param.isFirstPackage) return
